e762adcf5325a8200ee656c9fa9526266517ae39
Shivanshu <shivanshut105@gmail.com>
2023-08-17 02:28:33 +0530

    removed

--- burger-builder/src/components/UI/Backdrop/Backdrop.js
+++
@@ -1,9 +0,0 @@
-import classes from './Backdrop.module.css'
-
-const Backdrop = (props) => {
-    return ( 
-        props.show ? <div className={classes.Backdrop} onClick={props.close}></div> : null
-     );
-}
- 
-export default Backdrop;
