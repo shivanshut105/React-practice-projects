4d10d63e03f643705923697af9d780719f20fbbc
Shivanshu <shivanshut105@gmail.com>
2023-08-17 02:31:46 +0530

    add burger-builder project

---
+++ burger/src/components/UI/Backdrop/Backdrop.js
@@ -0,0 +1,9 @@
+import classes from './Backdrop.module.css'
+
+const Backdrop = (props) => {
+    return ( 
+        props.show ? <div className={classes.Backdrop} onClick={props.close}></div> : null
+     );
+}
+ 
+export default Backdrop;
